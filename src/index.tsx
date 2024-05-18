650939e46a337c9445a49227ac5e8b09cc279f4d
shagil786 <shagil.nizami@fincity.com>
2024-05-19 02:50:04 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MESUREMENT_ID,
+  apiKey: "AIzaSyA5Y6JNXFtJRXK5PfM5gvHVe3vxpRF0rtk",
+  authDomain: "react-js-91371.firebaseapp.com",
+  projectId: "react-js-91371",
+  storageBucket: "react-js-91371.appspot.com",
+  messagingSenderId: "433381353324",
+  appId: "1:433381353324:web:f1d8d256b7d416d4ca7549",
+  measurementId: "G-LMWV7K0N58",
 };
 
 const app = initializeApp(firebaseConfig);
