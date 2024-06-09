0f4420da933d1ed506509c1d96287b1ff4b14e61
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2024-06-09 09:55:48 +0530

    changes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,4 +3,4 @@ import axios from "axios";
 export const getCountries = ({ endpoint, searchText }: {
     endpoint: string,
     searchText?: string,
-}) => axios.get(`https://restcountries.com/v3.1/${endpoint ? `${endpoint}/${searchText ?? ""}` : ``}`).then(({ data }) => data);
+}) => axios.get(`https://countries-p4z0.onrender.com/v3.1/${endpoint ? `${endpoint}/${searchText ?? ""}` : ``}`).then(({ data }) => data);
